716e35ea1eacdd170c78945c0ab2f69d3c494471
Ashfak chowdhury <ashfak.chowdhury@bjitgroup.com>
2017-03-30 12:36:12 +0600

    first commit

---
+++ src/main/java/com/test/SpringHibernate/service/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.test.SpringHibernate.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.test.SpringHibernate.dao.EmployeeDAO;
+import com.test.SpringHibernate.model.Employee;
+
+@Service
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDAO employeeDAO;
+
+	@Override
+	@Transactional
+	public void addEmployee(Employee employee) {
+		employeeDAO.addEmployee(employee);
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> getAllEmployees() {
+		return employeeDAO.getAllEmployees();
+	}
+
+	@Override
+	@Transactional
+	public void deleteEmployee(Integer employeeId) {
+		employeeDAO.deleteEmployee(employeeId);
+	}
+
+	public Employee getEmployee(int empid) {
+		return employeeDAO.getEmployee(empid);
+	}
+
+	public Employee updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return employeeDAO.updateEmployee(employee);
+	}
+
+	public void setEmployeeDAO(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+}
